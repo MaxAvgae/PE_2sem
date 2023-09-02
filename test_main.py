6810413f31404a607f59997751fa0ade6d9e59ac
MaxAvgae <maxavagerus@gmail.com>
2023-09-02 23:52:21 +0300

    ppppp

--- test_main.py
+++ test_main.py
@@ -1,34 +1,37 @@
 from fastapi.testclient import TestClient
-from main import app  # копируем наше "приложение" из main
+from main import app #копируем наше "приложение" из main  
 
 
 client = TestClient(app)
 
 
 def test_root_main():
-    response = client.get("/")
-    # проверяем код ответа
+    response = client.get('/')
+    #проверяем код ответа
     assert response.status_code == 200
-    # и сам ответ
-    assert response.json() == {"messege": "Ohh. It's working"}
+    #и сам ответ
+    assert response.json() == {'messege': "Ohh. It's working"}
 
 
 def test_predict_positive():
-    response_post = client.post("/predict/", json={"text": "I like PE"})
+    response_post = client.post('/predict/',
+                                json = {'text': 'I like PE'})
     json_data = response_post.json()
     assert response_post.status_code == 200
-    assert json_data["label"] == "POSITIVE"
+    assert json_data['label'] == 'POSITIVE'
 
 
 def test_predict_negative():
-    response_post = client.post("/predict/", json={"text": "I hate PE"})
+    response_post = client.post('/predict/',
+                                json = {'text': 'I hate PE'})
     json_data = response_post.json()
     assert response_post.status_code == 200
-    assert json_data["label"] == "NEGATIVE"
-
-
-def test_predict_neutral():
-    response_post = client.post("/predict/", json={"text": "It's PE"})
+    assert json_data['label'] == 'NEGATIVE'
+    
+def test_predict_neg_2():
+    response_post = client.post('/predict/',
+                                json = {'text': "It's PE"})
     json_data = response_post.json()
     assert response_post.status_code == 200
-    assert json_data["label"] == "NEUTRAL"
+    assert json_data['label'] == 'NEGATIVE' 
+    
